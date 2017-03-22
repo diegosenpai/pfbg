050e8b554c7ecf10dfaeaffecd917b16cf494522
diegosenpai <dmalmeida@gmail.com>
2017-03-21 21:05:18 -0500

    Proyeto capitulo Uno Agregar editor para probar configuracion PF Primer formulario basico.

---
+++ pf-beginners-guide/pf-beginners-guide/src/main/java/ec/com/orion/learn/pfbg/controller/UserController.java
@@ -0,0 +1,36 @@
+package ec.com.orion.learn.pfbg.controller;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+
+import ec.com.orion.learn.pfbg.model.User;
+
+@ManagedBean
+@RequestScoped
+public class UserController {
+
+	private User registrationUser;
+
+	public UserController() {
+		registrationUser = new User();
+	}
+
+	public User getRegistrationUser() {
+		return registrationUser;
+	}
+
+	public void setRegistrationUser(User registrationUser) {
+		this.registrationUser = registrationUser;
+	}
+	
+	public String register(){
+		System.out.println("registering user: "+this.registrationUser);
+		String msg = "User registered successfully";
+		FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO,msg,msg));
+		FacesContext.getCurrentInstance().getExternalContext().getFlash().setKeepMessages(true);
+		return "registration.jsf?faces-redirect=true";
+	}
+
+}
